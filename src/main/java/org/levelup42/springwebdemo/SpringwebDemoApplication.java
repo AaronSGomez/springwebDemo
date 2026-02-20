e540da514668cb4a1ba609387080fd2f1ad3a282
Aarón Santiago Gómez Abella <nomeacuerdobien@gmail.com>
2026-02-21 00:41:32 +0100

    Initial commit

---
+++ src/main/java/org/levelup42/springwebdemo/SpringwebDemoApplication.java
@@ -0,0 +1,13 @@
+package org.levelup42.springwebdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringwebDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/levelup42/springwebdemo/SpringwebDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.levelup42.springwebdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
